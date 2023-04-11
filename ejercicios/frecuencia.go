686455131c519289640898ec752ca9db398d4a51
gonza <cardoso63263@estudiantes.edu.ar>
2023-04-11 10:15:30 -0300

    solucion 1

--- ejercicios/frecuencia.go
+++ ejercicios/frecuencia.go
@@ -2,8 +2,22 @@ package ejercicios
 
 import (
 	"guia6/dictionary"
+
+	"github.com/agrison/go-commons-lang/stringUtils"
 )
 
-func Frecuencia(s string) *dictionary.Dictionary[string, int]{
-	panic("Not implemented")
+func Frecuencia(s string) *dictionary.Dictionary[string, int] {
+	dict := dictionary.NewDictionary[string, int]()
+	for _, letra := range s {
+		if stringUtils.IsAlpha(string(letra)) {
+
+			var frecuencia int = 1
+			if dict.Contains(string(letra)) {
+				frecuencia = dict.Get(string(letra))
+				frecuencia++
+			}
+			dict.Put(string(letra), frecuencia)
+		}
+	}
+	return &dict
 }
